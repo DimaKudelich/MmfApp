ce5468f738eeae847630e86bce7f6fa3235b7529
Dmitry <dima.kudelich2@mail.ru>
2022-06-10 19:32:24 +0300

    remove old color names

--- app/src/main/java/com/kudelich/mmfapp/ui/theme/Color.kt
+++ app/src/main/java/com/kudelich/mmfapp/ui/theme/Color.kt
@@ -2,7 +2,6 @@ package com.kudelich.mmfapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF18806E)//топбар
-val Purple700 = Color(0xFF18806E)//грань выше топбара
-val Teal200 = Color(0xFF03DAC5)
+val GreenLight = Color(0xFF18806E)
+val GreenDark = Color(0xFF18806E)
+val Teal = Color(0xFF03DAC5)
